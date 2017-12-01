caa1bf423d6fc80497a0eb32a714f7056cd00d24
ranjan hegde <hegde.spurthi@gmail.com>
2017-12-01 19:49:24 +0400

    Working version of the Application.

--- js/app.js
+++ js/app.js
@@ -17,6 +17,26 @@
 // 	// 	return false;
 // 	// 	})
 // });
-window.ready(function(){
- 	 alert('hello');
- });
+
+
+// $(function () {
+// 	var offset = 250;
+// 	var duration = 100;
+// 	$(window).scroll(function() {
+// 		// console.log($(this).scrollTop());
+// 		if ($(this).scrollTop() > offset) 
+// 		{
+// 		$(".back-to-top").fadeIn(300); 
+// 		}
+// 		else
+// 		{ 
+// 		$(".back-to-top").fadeOut(300); 
+// 		}	
+// 	});
+
+// 	$('.back-to-top').click(function(event) {
+// 		event.preventDefault();
+// 		$('html, body').animate({scrollTop: 0}, duration);
+// 		return false;
+// 		});
+// });
