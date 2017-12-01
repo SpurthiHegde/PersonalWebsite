ba8a99322ded4d6cf209994035e576a050bbca0a
ranjan hegde <hegde.spurthi@gmail.com>
2017-12-01 19:17:16 +0400

    Fine tuned- followed Project Rubric

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// $(document).ready(function() {
+// 	// var offset = 250;
+// 	// var duration = 100;
+// 	alert('hello');
+// 	// $(window).scroll(function() {
+// 	// 	// console.log($(this).scrollTop());
+//  // 		if ($(this).scrollTop() > offset) 
+//  // 		{ 
+//  // 		$(".back-to-top").fadeIn(300); } 
+//  // 		else 
+//  // 		{ $(".back-to-top").fadeOut(300); }
+// 	// });
+ 
+// 	// $('.back-to-top').click(function(event) {
+// 	// 	event.preventDefault();
+// 	// 	$('html, body').animate({scrollTop: 0}, duration);
+// 	// 	return false;
+// 	// 	})
+// });
+window.ready(function(){
+ 	 alert('hello');
+ });
